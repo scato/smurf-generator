de958565b9e61bb153adf363e55a1a7b21ab3a97
Scato Eggen <scato.eggen@gmail.com>
2017-04-02 15:01:57 +0200

    Make generate.sh POSIX compatible

--- generate.sh
+++ generate.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+toBaseName='{ split($0, parts, "/"); len = length(parts); print parts[len] }'
+toCamelCase='{ print tolower(substr($0, 1, 1)) substr($0, 2) }'
+toUpperCase='{ rest = substr($0, 2); gsub(/[A-Z]/, "_&", rest); print substr($0, 1, 1) toupper(rest) }'
+
 target=$1
-artifact=$(echo $target | sed "s/^.*\\/\(.*\)$/\\1/")
+artifact=$(echo $target | sed -e 's/^.*\/\(.*\)$/\1/')
 examples=$(cd examples; find * -type f)
 
 for example in $examples
 do
     src="examples/$example"
-    regexp=$(echo $example | sed "s/\\./\\\\./g;s/Smurf/\\\\(.*\\\\)/g")
+    regexp=$(echo $example | sed -e 's/\./\\./g' -e 's/Smurf/\\(.*\\)/g')
     match=$(echo $artifact | grep "$regexp" | sed "s/$regexp/\\1/")
 
     if [ "$match" != "" ]
     then
         pascalCase=$match
-        camelCase=$(echo $pascalCase | gsed -r "s/^./\L\0/")
-        upperCase=$(echo $camelCase | gsed -r "s/[A-Z]/_\0/g;s/./\U\0/g")
+        camelCase=$(echo $pascalCase | awk "$toCamelCase")
+        upperCase=$(echo $pascalCase | awk "$toUpperCase")
 
-        cat $src | sed "s/Smurf/$pascalCase/g;s/smurf/$camelCase/g;s/SMURF/$upperCase/g" > $target
+        cat $src | sed -e "s/Smurf/$pascalCase/g" -e "s/smurf/$camelCase/g" -e "s/SMURF/$upperCase/g" > $target
     fi
 done
