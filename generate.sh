1f032feca10f477700c8049ec655354db1754026
Scato Eggen <scato.eggen@gmail.com>
2017-04-02 15:23:36 +0200

    Improve variable naming

--- generate.sh
+++ generate.sh
@@ -4,14 +4,14 @@ toBaseName='{ split($0, parts, "/"); len = length(parts); print parts[len] }'
 toCamelCase='{ print tolower(substr($0, 1, 1)) substr($0, 2) }'
 toUpperCase='{ rest = substr($0, 2); gsub(/[A-Z]/, "_&", rest); print substr($0, 1, 1) toupper(rest) }'
 
-target=$1
-artifact=$(echo $target | sed -e 's/^.*\/\(.*\)$/\1/')
-examples=$(cd examples; find * -type f)
+output=$1
+artifact=$(echo $output | sed -e 's/^.*\/\(.*\)$/\1/')
+templates=$(cd templates; find * -type f)
 
-for example in $examples
+for template in $templates
 do
-    src="examples/$example"
-    regexp=$(echo $example | sed -e 's/\./\\./g' -e 's/Smurf/\\(.*\\)/g')
+    input="examples/$template"
+    regexp=$(echo $template | sed -e 's/\./\\./g' -e 's/Smurf/\\(.*\\)/g')
     match=$(echo $artifact | grep "$regexp" | sed "s/$regexp/\\1/")
 
     if [ "$match" != "" ]
@@ -20,6 +20,6 @@ do
         camelCase=$(echo $pascalCase | awk "$toCamelCase")
         upperCase=$(echo $pascalCase | awk "$toUpperCase")
 
-        cat $src | sed -e "s/Smurf/$pascalCase/g" -e "s/smurf/$camelCase/g" -e "s/SMURF/$upperCase/g" > $target
+        cat $input | sed -e "s/Smurf/$pascalCase/g" -e "s/smurf/$camelCase/g" -e "s/SMURF/$upperCase/g" > $output
     fi
 done
